63884e0a754c2dce81822985bc640e6b93379f75
wzngekj <147592707+wzngekj@users.noreply.github.com>
2024-01-23 01:51:43 +0800

    web

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("photo").addEventListener('click', function() {
+        document.getElementById("hidden").style.display='block';
+    });
+})
